ba45dc2fe86b9bbbc18dfd83cc05234edbe970e7
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-05-16 11:52:20 +0530

    add new user

---
+++ Web Chat/newuser.php
@@ -0,0 +1,25 @@
+   
+                    <form action="signup.php" method="post">
+                    <h2>Don't have an account sign up here</h2>
+                    
+                    <table>
+                    <tr>
+                    <td><label>User Name:</label></td>
+                    <td><input type="text" name="uname" placeholder="UserName"></td>
+                    </tr>
+
+                    <tr>
+                    <td><label>Email:</label></td>
+                    <td><input type="text" name="Email" placeholder="Email"></td>
+                    </tr>
+
+                    <tr>
+                    <td><label>Password:</label></td>
+                    <td><input type="text" name="Password" placeholder="Password"></td>
+                    </tr>
+                    </table>
+                    
+                    <button style="background-color: #6495ed; color: white;" 
+                    type="submit"><b>signup</b></button>
+                    </form>
+ 
