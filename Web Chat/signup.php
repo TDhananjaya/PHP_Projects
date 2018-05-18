27c87bbeb02b853d8ac11e4503407573383c7eff
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-05-18 22:21:16 +0530

    Update signup

--- Web Chat/signup.php
+++ Web Chat/signup.php
@@ -1,11 +1,12 @@
 <?php
+
     include 'dbh.php';
     $uname = $_POST['uname'];
     $email = $_POST['Email'];
     $pass = $_POST['Password'];
 
     $sql="insert into signup(username,email,password)
-    values('$uname', '$email','$pass')"
+    values('$uname', '$email','$pass')" ;
 
     $result =$conn->query($sql);
 
